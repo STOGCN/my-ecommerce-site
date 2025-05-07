98e2225ddcf6f8a19f41c6c0c0a42ba0db682f96
T672110141 <systemofgodcoednature@gmail.com>
2025-05-07 16:50:07 +0700

    session5

---
+++ expServer.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path  = require('path');
+
+const app = express();
+//server static files from current projject folder
+app.use(express.static(__dirname));
+
+app.get('/',() => {
+resizeBy.senFile(path.join(__dirname,"index.html"))
+});
+app.listen(3000,() => {
+console.log("Server is running at http://localhost:3000")
+});
---
+++ subject.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const  server = http.createServer(function (req, res) {  
+
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST')
+    res.setHeader('Access-Control-Allow-Header','Content-Type')
+
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    res.write('{"contactSubject": ["General Enquiry","Class","Schedule","Intructor","Price","Oyher"]}');
+    res.end();
+
+
+ });
+
+
+ server.listen(8080);
